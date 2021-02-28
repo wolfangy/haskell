46e16fc3cdf19192b7e2a5cc311846ce9c5d898c
Juanyong YANG <juanyong.yang@gmail.com>
2021-02-27 22:24:16 -0500

    add more examples for arbitrary & Gen

--- FromFirstPrinciples/Ch14_Testing/trivial.hs
+++ FromFirstPrinciples/Ch14_Testing/trivial.hs
@@ -7,6 +7,51 @@ module Main where
 --
 import Test.QuickCheck
 
+trivialInt :: Gen Int
+trivialInt = return 1
+
+oneThroughThree :: Gen Int
+oneThroughThree = elements [1,2,3]
+
+oneThroughThree' :: Gen Int
+oneThroughThree' = elements [1,2,2,2,3]
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a , Arbitrary b) => Gen (a, b)
+genTuple = do
+    a <- arbitrary
+    b <- arbitrary
+    return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a, b, c)
+genThreeple = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [Left a, Right b]
+
+-- Use the `choose` and `elements` from QuickCheck as generator of values
+--
+
+
 data Trivial = Trivial deriving (Eq, Show)
 
 trivialGen :: Gen Trivial

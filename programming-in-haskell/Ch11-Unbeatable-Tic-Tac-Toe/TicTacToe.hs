063858f2704a3186d483ad54b92efd6ef9ccf4b4
Juanyong YANG <juanyong.yang@gmail.com>
2022-02-23 22:45:54 -0500

    ch11 first 3 sections

---
+++ programming-in-haskell/Ch11-Unbeatable-Tic-Tac-Toe/TicTacToe.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE GADTs #-}
+
+module TicTacToe where
+
+import Data.Char
+import Data.List
+import System.IO
+
+-- 11.2 Basic declarations
+
+size :: Int
+size = 3
+
+type Grid = [[Player]]
+
+data Player where
+    O :: Player
+    B :: Player
+    X :: Player
+    deriving (Eq, Ord, Show)
+
+next :: Player -> Player
+next O = X
+next B = B
+next X = O
+
+-- 11.3 Grid utilities
+
+empty :: Grid
+empty = replicate size $ replicate size B
+
+full :: Grid -> Bool
+full = notElem B . concat
+
+turn :: Grid -> Player
+turn g = if os < xs then O else X
+    where
+        os = length . filter (== O) $ ps
+        xs = length . filter (== X) $ ps
+        ps = concat g
+
+wins :: Player -> Grid -> Bool
+wins player grid = any line (rows ++ cols ++ diagonals)
+    where
+        line = all (== player)
+        rows = grid
+        cols = transpose grid
+        diagonals = [diag grid, diag (map reverse g)]
+
+diag :: Grid -> [Player]
+diag g = [g !! n !! n | n <- [0..size - 1]]
+
+won :: Grid -> Bool
+won g = wins O g || wins X g
+
+
+-- 11.4 Displaying a grid
+
+putGrid :: Grid -> IO ()
+putGrid =
+    putStrLn . unlines . concat . interleave bar . map showRow
+    where
+        bar = [replicate ((size * 4) - 1) '-']
+
+showRow :: [Player] -> [String]
+showRow = beside . interleave bar . map showPlayer
+    where
+        beside = foldr1 (zipWith (++)) 
+        bar    = replicate 3 "|"
+
+
+showPlayer :: Player -> [String]
+showPlayer O = ["   ", " O ", "   "]
+showPlayer B = ["   ", "   ", "   "]
+showPlayer X = ["   ", " X ", "   "]
+
+interleave :: a -> [a] -> [a]
+interleave _ []     = []
+interleave n [x]    = [x]
+interleave n (x:xs) = x : n : interleave n xs

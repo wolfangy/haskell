0c170ae60d1de1cf3036a6ea357ac4233fc71a47
Juanyong YANG <juanyong.yang@gmail.com>
2022-08-25 22:23:23 -0400

    Update user-number-range.hs

--- challenges/user-number-range.hs
+++ challenges/user-number-range.hs
@@ -2,13 +2,14 @@ import Control.Monad
 
 main :: IO ()
 main = do
-    forM_ (rangeUser users) putStrLn
+    forM_ (rangeUserYang users) putStrLn
     putStrLn ""
-    forM_ (rangeUser' users) putStrLn
+    forM_ (rangeUserFrancisco users) putStrLn
     where
         users = [1, 5, 10, 15, 20, 25]
 
-rangeUser = snd . rangeUserImpl . reverse
+rangeUserYang :: [Int] -> [String]
+rangeUserYang = snd . rangeUserImpl . reverse
 
 rangeUserImpl :: [Int] -> (Int, [String])
 rangeUserImpl [] = error "At least one element"
@@ -18,6 +19,5 @@ rangeUserImpl (x:xs) =
         (prev, output) = rangeUserImpl xs
     in (x, output ++ [show (prev + 1) ++ ".." ++ show x])
 
-rangeUser' :: [Int] -> [String]
-rangeUser' xs = [ if y == -1 then show x else show (x + 1) ++ ".." ++ show y | 
-                (x, y) <- (head xs, negate 1) : zip xs (tail xs)]
+rangeUserFrancisco :: [Int] -> [String]
+rangeUserFrancisco xs = [ if y == -1 then show x else show (x + 1) ++ ".." ++ show y | (x, y) <- (head xs, negate 1) : zip xs (tail xs)]
